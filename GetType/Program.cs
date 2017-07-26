ee638685070125abac190d3b7120176eb92934fb
NameInikita <name.inikita@gmail.com>
2017-07-27 00:07:51 +0300

    GetType на экземпляре класса

---
+++ GetType/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GetType
+{
+    class MyClass
+    {
+        private int field;
+
+        int Field { get; set; }
+        public MyClass()
+        {
+        }
+        public MyClass(int field)
+        {
+            this.field = field;
+        }
+        private int Method(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass my = new MyClass();
+            Type type;
+
+            type = my.GetType();
+            Console.WriteLine(type);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
